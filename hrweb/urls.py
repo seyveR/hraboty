adfacfa40b7d2660deed0b526585baacceb29041
Vladimir <AkVoPF@mail.ru>
2024-02-20 23:20:30 +0300

    Initial commit

---
+++ hrweb/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path 
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about_us', views.about_us, name='about_us'),
+    path('price', views.price, name='price'),
+    path('profile', views.profile, name='profile'),
+    path('auth', views.auth, name='auth'),
+    path('reg', views.reg, name='reg'),
+]
---
+++ hrweb/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def home(request): 
+    return render(request, 'home.html')
+
+
+def about_us(request): 
+    return render(request, 'aboutus.html')
+
+def price(request): 
+    return render(request, 'price.html')
+
+def profile(request): 
+    return render(request, 'profile.html')
+
+def auth(request):
+    return render(request, 'auth.html')
+
+def reg(request):
+    return render(request, 'reg.html')
